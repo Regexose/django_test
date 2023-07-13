b661dc44041f96f4e738e439a29947337ed1bae2
Regexose <51819860+Regexose@users.noreply.github.com>
2023-07-13 09:00:20 +0200

    added bilingual fields to model

--- mysite/projects02/models.py
+++ mysite/projects02/models.py
@@ -2,8 +2,12 @@ from django.db import models
 #from https://soshace.com/upload-multiple-images-to-a-django-model-without-plugins/
 
 class Project(models.Model):
-    title = models.CharField(max_length=250)
-    description = models.TextField()
+    title = models.CharField(max_length=250, blank=True)
+    titel= models.CharField(max_length=250, blank=True)
+    short_description = models.CharField(max_length=250, blank=True)
+    kurzbeschreibung = models.CharField(max_length=250, blank=True)
+    description = models.TextField(blank=True)
+    beschreibung = models.TextField(blank=True)
     image = models.FileField(blank=True)
 
     def __str__(self):
