18648f886a091190eaccd9892e5dc7cf53c14906
Regexose <51819860+Regexose@users.noreply.github.com>
2023-07-15 09:25:40 +0200

    added django ckeditor for html editor

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'polls.apps.PollsConfig',
     'projects',
     'projects02',
+    'ckeditor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
