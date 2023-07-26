f3bf0640497212a6e6a85ff6fb95c06f761dbc39
Regexose <51819860+Regexose@users.noreply.github.com>
2023-07-26 15:41:35 +0200

    index page with "featured project"
    added featured to Project model

--- mysite/projects02/urls.py
+++ mysite/projects02/urls.py
@@ -8,5 +8,6 @@ from . import views
 app_name = "projects02"
 urlpatterns = [
                   path('', views.project_view, name='project'),
+                  path('home/', views.index_view, name='index'),
                   path('<int:id>/', views.detail_view, name='detail')
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mysite/projects02/views.py
+++ mysite/projects02/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Project, ProjectImage, ProjectEmbed
+import random
+
+def index_view(request):
+    featured = Project.objects.get(featured='Y')
+    projects = Project.objects.all()
+    context = {'projects': projects,
+               'featured': featured
+               }
+    return render(request, 'index.html', context)
 
 def project_view(request):
     projects = Project.objects.all()
@@ -7,10 +16,12 @@ def project_view(request):
 
 
 def detail_view(request, id):
+    projects = Project.objects.all()
     project = get_object_or_404(Project, id=id)
     photos = ProjectImage.objects.filter(project=project)
     embeds = ProjectEmbed.objects.filter(project=project)
     return render(request, 'detail_grid.html', {
+        'projects': projects,
         'project': project,
         'photos': photos,
         'embeds': embeds
