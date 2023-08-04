e80f9700b80514bcf9aae7e871c5a4d83a039b43
Regexose <51819860+Regexose@users.noreply.github.com>
2023-08-04 15:16:09 +0200

    Another wsgi update

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -11,8 +11,12 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(BASE_DIR)
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 #path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.environ('DJANGO_SETTINGS_MODULE', 'mysite.settings')
